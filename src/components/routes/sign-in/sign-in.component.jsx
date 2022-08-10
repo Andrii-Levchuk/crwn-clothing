4c9c9f8757db5aac25e103a015c0f8caf4c6aecd
AndrLevchuk <Andriykolevchuk@gmail.com>
2022-08-10 14:02:53 +0200

    created sign up componet, the form input component, button component

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -2,22 +2,24 @@ import {
 	signInWithGooglePopup,
 	createUserDocumentFromAuth,
 } from '../../../utils/frebase/firebase,utils'
+import SignUpForm from '../../sign-up-form/sign-up-form.component'
 
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup()
-    const userDocRef = await createUserDocumentFromAuth(user)
-  }
 
+
+	const logGoogleUser = async () => {
+		const { user } = await signInWithGooglePopup()
+		const userDocRef = await createUserDocumentFromAuth(user)
+	}
 
 	return (
 		<div>
 			<h1>Sign In Page</h1>
 			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
+			<SignUpForm />
 		</div>
 	)
 }
-
 
 export default SignIn
