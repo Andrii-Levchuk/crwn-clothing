8432c803a6d778f426efa41514e0589aa514e3f3
AndrLevchuk <Andriykolevchuk@gmail.com>
2022-08-13 10:57:18 +0200

    adding cart context and shopping items as well as cart dropdown

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,10 +1,14 @@
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import './navigation.styles.scss'
 
 import { ReactComponent as CrwnLogo} from '../../../assets/crown (1).svg'
+import { UserContext } from '../../../context/user.context'
 
 const Navigation = () => {
+
+	const {currentUser} = useContext(UserContext)
+	console.log(currentUser)
 	return (
 		<Fragment>
 			<div className='navigation'>
